6bab70a78ea16b73dc747091850758b437b5a47d
Tiexin Guo <guotiexin@gmail.com>
2024-06-18 11:18:18 +0800

    test

--- tests/interface/conftest.py
+++ tests/interface/conftest.py
@@ -2,18 +2,22 @@
 # See LICENSE file for licensing details.
 
 import pytest
-from charm import OathkeeperCharm
+from unittest.mock import patch
+
 from interface_tester import InterfaceTester
 from scenario.state import State
+
+from charm import OathkeeperCharm
 
 
 @pytest.fixture
 def interface_tester(interface_tester: InterfaceTester):
-    interface_tester.configure(
-        charm_type=OathkeeperCharm,
-        state_template=State(
-            leader=True,
-            config={"dev": True},
-        ),
-    )
-    yield interface_tester
+    with patch("charm.KubernetesServicePatch", lambda x, y: None):
+        interface_tester.configure(
+            charm_type=OathkeeperCharm,
+            state_template=State(
+                leader=True,
+                config={"dev": True},
+            ),
+        )
+        yield interface_tester
